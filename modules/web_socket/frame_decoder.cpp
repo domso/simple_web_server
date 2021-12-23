0bbf078a7f20035a7ae0c5232a51ab6df7d2e5ca
domso <domso94@googlemail.com>
2021-12-23 18:28:13 +0100

    small fix

--- modules/web_socket/frame_decoder.cpp
+++ modules/web_socket/frame_decoder.cpp
@@ -20,14 +20,14 @@ size_t web_server::modules::web_socket::frame_decoder::unpack_data(
             data_length = (region.data()[2] << 8) | region.data()[3];
             header_length = 4;
         } else if (byte_len == 127 && region.size() >= 10) {
-            data_length = (static_cast<size_t>(region.data()[2]) << 56) | 
-                        (static_cast<size_t>(region.data()[3]) << 48) |
-                        (static_cast<size_t>(region.data()[4]) << 40) | 
-                        (static_cast<size_t>(region.data()[5]) << 32) | 
-                        (static_cast<size_t>(region.data()[6]) << 24) | 
-                        (static_cast<size_t>(region.data()[7]) << 16) | 
-                        (static_cast<size_t>(region.data()[8]) << 8)  |
-                        static_cast<size_t>(region.data()[9]);
+            data_length = (static_cast<uint64_t>(region.data()[2]) << 56) | 
+                        (static_cast<uint64_t>(region.data()[3]) << 48) |
+                        (static_cast<uint64_t>(region.data()[4]) << 40) | 
+                        (static_cast<uint64_t>(region.data()[5]) << 32) | 
+                        (static_cast<uint64_t>(region.data()[6]) << 24) | 
+                        (static_cast<uint64_t>(region.data()[7]) << 16) | 
+                        (static_cast<uint64_t>(region.data()[8]) << 8)  |
+                        static_cast<uint64_t>(region.data()[9]);
             header_length = 10;
         } else {
             return 0;
