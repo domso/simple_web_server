a52d31451b4b61a58ee2e62fdf058bc7182e2061
domso <domso94@googlemail.com>
2021-12-23 19:27:55 +0100

    maybe fix

--- web_server/http/requester.h
+++ web_server/http/requester.h
@@ -30,7 +30,7 @@ namespace web_server::http {
             internal_module new_module;
             new_module.name = name;
             new_module.request_callback = [&mod](const std::unordered_map<std::string, std::string>& request, std::unordered_map<std::string, std::string>& response, const std::string& res, const config& current_config) {
-                return mod.request_callback(request, response, res, current_config);
+                return std::move(mod.request_callback(request, response, res, current_config));
             };
             new_module.authentication = {username, password};
             
@@ -42,7 +42,7 @@ namespace web_server::http {
             internal_module new_module;
             new_module.name = name;
             new_module.request_callback = [&mod](const std::unordered_map<std::string, std::string>& request, std::unordered_map<std::string, std::string>& response, const std::string& res, const config& current_config) {
-                return mod.request_callback(request, response, res, current_config);
+                return std::move(mod.request_callback(request, response, res, current_config));
             };
             new_module.native_callback = [&mod](native::handle& handle) {
                 mod.native_callback(handle);
